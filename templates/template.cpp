ef20aeac6d7c0416e1bd818046d6cd4d7f85a822
Edwin Guzman <vudduu@gmail.com>
2017-03-09 22:17:07 -0400

    add cpp template

---
+++ templates/template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+#define FOR(i,a,b)  for(int i=(a),_##i=(b);i<_##i;++i)
+#define F(i,a)      FOR(i,0,a)
+#define ALL(x)      x.begin(),x.end()
+#define PB          push_back
+#define S           size()
+#define MP          make_pair
+
+typedef long long   LL;
+
+int main() {
+}
